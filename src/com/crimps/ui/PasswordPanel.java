ead4c69ab7622c4099ec6ca14f70f49f0f9eeee8
crimps <wzchen@travelsky.com>
2017-09-30 14:22:19 +0800

    密码面板

---
+++ src/com/crimps/ui/PasswordPanel.java
@@ -0,0 +1,13 @@
+package com.crimps.ui;
+
+import javax.swing.*;
+
+/**
+ * Created by crimps on 2017/9/30.
+ */
+public class PasswordPanel extends JPanel {
+
+    public PasswordPanel() {
+
+    }
+}
