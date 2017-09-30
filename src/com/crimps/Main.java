e4b0149736d7a7b2caed083ddc88fadf272d10ab
crimps <wzchen@travelsky.com>
2017-09-30 10:42:07 +0800

    初始化工程

---
+++ src/com/crimps/Main.java
@@ -0,0 +1,25 @@
+package com.crimps;
+
+import com.crimps.ui.MainUI;
+import org.jb2011.lnf.beautyeye.BeautyEyeLNFHelper;
+
+import java.awt.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try {
+            BeautyEyeLNFHelper.frameBorderStyle = BeautyEyeLNFHelper.FrameBorderStyle.translucencyAppleLike;
+            org.jb2011.lnf.beautyeye.BeautyEyeLNFHelper.launchBeautyEyeLNF();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                MainUI mainUI = new MainUI();
+                mainUI.setVisible(true);
+            }
+        });
+    }
+}
---
+++ src/com/crimps/ui/MainUI.java
@@ -0,0 +1,12 @@
+package com.crimps.ui;
+
+import javax.swing.*;
+
+/**
+ * Created by crimps on 2017/9/30.
+ */
+public class MainUI extends JFrame {
+    public MainUI() {
+        this.setSize(300, 200);
+    }
+}
